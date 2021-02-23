c7b1baf302215bede3b753f858b5eeb124bdf90d
Emiko <emi9361@hotmail.co.jp>
2021-02-23 21:40:46 +0900

    rev_seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-3.times do |n|
+  3.times do |n|
   restaurant = Restaurant.new(
-    name: "testレストラン_＃{n}",
+    name: "testレストラン_#{n}",
     fee: 100,
     time_required: 10,
   )
-end
 
   12.times do |m|
-
-    # buildを使うと既存のrestaurantと紐付けされているfoodから紐付けされる
     restaurant.foods.build(
       name: "フード名_#{m}",
       price: 500,
       description: "フード_#{m}の説明文です。"
     )
-
-    restaurant.save!
   end
+# !をつけると、返り値が違うのよー(例外を拾う為に使うため!!!)
+
+  restaurant.save!
+end
+
